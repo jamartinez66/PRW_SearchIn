5928969e1dda53304cb0eab840013d1ec137204d
Jose Antonio Martínez Ramos <joseamartinezr@gmail.com>
2023-03-07 23:00:17 +0000

    Delete comments

--- searchin/contacto.php
+++ searchin/contacto.php
@@ -8,29 +8,25 @@
     <title>Search-In: Contacto</title>
 </head>
 <body>
-    <!-- <div id="contenido">
-        <div id="cuadro"> -->
-            <div class="formulario">
-                <h2>Contacto</h2> 
-                <!-- Formulario para insertar el correo y enviarlo posteriormente -->
-                <form action="enviar.php" method="post">
-                    <label for="nombre">Nombre:</label>
-                    <input type="text" id="nombre" name="nombre" required>
+    <div class="formulario">
+        <h2>Contacto</h2> 
+        <!-- Formulario para insertar el correo y enviarlo posteriormente -->
+        <form action="enviar.php" method="post">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required>
 
-                    <label for="email">Correo electrónico:</label>
-                    <input type="email" id="email" name="email" required>
+            <label for="email">Correo electrónico:</label>
+            <input type="email" id="email" name="email" required>
 
-                    <label for="telefono">Número de teléfono:</label>
-                    <input type="tel" id="telefono" name="telefono" required>
+            <label for="telefono">Número de teléfono:</label>
+            <input type="tel" id="telefono" name="telefono" required>
 
-                    <label for="motivo">Motivo:</label>
-                    <textarea id="motivo" name="motivo" required></textarea>
+            <label for="motivo">Motivo:</label>
+            <textarea id="motivo" name="motivo" required></textarea>
 
-                    <input type="submit" value="Enviar">
-                </form>
-                <a href="index.php">Volver al inicio</a>
-            </div>
-        <!-- </div>
-    </div> -->
+            <input type="submit" value="Enviar">
+        </form>
+        <a href="index.php">Volver al inicio</a>
+    </div>
 </body>
 </html>
