2cf861a65db515c6837ad3ada7ba9fee7e5d3050
Jose Antonio Martínez Ramos <joseamartinezr@gmail.com>
2023-03-07 22:39:44 +0000

    Contact form and Response form

--- searchin/contacto.php
+++ searchin/contacto.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/contacto.css">
+    <link rel="stylesheet" href="styles/contactar.css">
     <title>Search-In: Contacto</title>
 </head>
 <body>
     <div id="contenido">
         <div id="cuadro">
-            <div class="encabezado1"><!-- Cabecera  -->
-                <p style="text-decoration: underline">Contacto</p>
-                <p style="font-size: 1rem">Rellene el campo de su correo y pulse enviar para contactar con nosotros:</p>
-            </div>
             <div id="central"><!-- Formulario para insertar el correo y enviarlo posteriormente -->
-                <form action="mailto:josearidane@gmail.com" method="GET">
-                    <input type="text" id="nombre" name="email" value="" placeholder="Introduce tu email">
-               
-                <div id="botones"><!-- Botones de navegación  -->
-                    <input type="submit" id="enviar" name="enviar" value="Enviar Email">
-                    <a href="./index.php" id="inicio">Inicio</a>
-                </div>
+                <form action="enviar.php" method="post">
+                    <label for="nombre">Nombre:</label>
+                    <input type="text" id="nombre" name="nombre" required>
+
+                    <label for="email">Correo electrónico:</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="telefono">Número de teléfono:</label>
+                    <input type="tel" id="telefono" name="telefono" required>
+
+                    <label for="motivo">Motivo:</label>
+                    <textarea id="motivo" name="motivo" required></textarea>
+
+                    <input type="submit" value="Enviar">
                 </form>
             </div>
         </div>
---
+++ searchin/enviar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulario de contacto</title>
+	<link rel="stylesheet" type="text/css" href="styles/enviar.css">
+</head>
+<body>
+	<div class="mensaje">
+		<h2>Mensaje enviado correctamente</h2>
+		<p>Gracias por contactarnos, nos pondremos en contacto contigo lo antes posible.</p>
+		<a href="index.php">Volver al inicio</a>
+	</div>
+</body>
+</html>
