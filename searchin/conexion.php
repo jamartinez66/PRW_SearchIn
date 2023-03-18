fdbdcce9341d08c44d3cdb84367bede6cda13ce6
Jose Antonio Martínez Ramos <joseamartinezr@gmail.com>
2023-03-18 12:59:36 +0000

    Connect DB error log

--- searchin/conexion.php
+++ searchin/conexion.php
@@ -1,13 +1,18 @@
 <?php
     // Clase que permitirá establecer la conexión con la base de datos.
     class Conectar{
-        private $servidor = "localhost";
+        private $servidor = "localhost:3306";
         private $usuario = "root";
         private $bd = "searchin";
         private $password = "Aa123456";
 
         public function conexion(){
             $conexion = mysqli_connect($this->servidor, $this->usuario, $this->password, $this->bd);
+            // Verificar si la conexión fue exitosa
+            if ($conexion->connect_error) {
+                // Si no se pudo conectar, imprimir el mensaje de error
+                die("Conexión fallida: " . $conexion->connect_error);
+            }
 
             return $conexion;
         }
